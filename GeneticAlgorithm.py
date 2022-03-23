54e79da7d25c840112bb581a712a2b24671e81e6
Pabilito <pborsukiewicz@onet.pl>
2022-03-23 11:03:43 +0100

    Binary vectors and two's compliment

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,4 +1,5 @@
 import random as ra
+import numpy as np
 
 class Algorithm:
     def __init__(self, inputData):
@@ -8,16 +9,38 @@ class Algorithm:
         self.population = []
         
     def __InitializePopulation(self):
-        for _ in range(self.Data[5]):                     #Population size
+        for _ in range(self.Data[5]):                               #Population size
             self.x = []
-            for dim in range(self.Data[0]):                 #One x for each dimension
+            for dim in range(self.Data[0]):                         #One x for each dimension
                 searchedInt = pow(2, self.Data[1][dim])
-                self.x.append(ra.randrange(-1*searchedInt, searchedInt-1))
+                randomInt = (ra.randrange(-1*searchedInt, searchedInt - 1))
+                binary = np.binary_repr(randomInt, self.Data[1][dim]+1)     #Conversion into two's compliment          
+                self.x.append(binary)
             self.population.append(self.x)
         return 
+
+    def __Crossover(self):
+        return
+
+    def __Mutation(self):
+        return
+
+    def __SurvivorSelection(self):
+        return
+
+    def __ReturnResultToUser(self):
+        return
+
+    def __LoopUntilTerminationCriterionReached(self):
+        for _ in range(self.Data[8]):
+            self.__Crossover()
+            self.__Mutation()
+            self.__SurvivorSelection()
+        return
 
     def PerformGeneticAlgorithm(self):
         self.__InitializePopulation()
         print(self.population)
-        #HERE WE WILL PERFORM FURTHER PHASES OF THE ALGORITHM
+        self.__LoopUntilTerminationCriterionReached()
+        self.__ReturnResultToUser()
         return
