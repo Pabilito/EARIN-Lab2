14c1a70b0b1f47e72a68c3a160c0020f22c469eb
Pabilito <pborsukiewicz@onet.pl>
2022-03-23 09:52:48 +0100

    Population initialization

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,13 +1,23 @@
+import random as ra
+
 class Algorithm:
     def __init__(self, inputData):
         #Order is: Dimensionality, Array of Range, Matrix A, Matrix B, c, Population size, Crossover, Mutation, Iterations
         #ExampleData = [2, [4, 5], np.array([[1., 2.],[3., 4.]]), np.array([[1.],[2.]]), 435.0, 234, 1.0, 0.123, 2334] 
         self.Data = inputData
+        self.population = []
         
     def __InitializePopulation(self):
+        for _ in range(self.Data[5]):                     #Population size
+            self.x = []
+            for dim in range(self.Data[0]):                 #One x for each dimension
+                searchedInt = pow(2, self.Data[1][dim])
+                self.x.append(ra.randrange(-1*searchedInt, searchedInt-1))
+            self.population.append(self.x)
         return 
 
     def PerformGeneticAlgorithm(self):
         self.__InitializePopulation()
+        print(self.population)
         #HERE WE WILL PERFORM FURTHER PHASES OF THE ALGORITHM
         return
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 def Main():
     ### Test data ###
     #Order is: Dimensionality, Array of Range, Matrix A, Matrix B, c, Population size, Crossover, Mutation, Iterations
-    DataFromUser = [2, [4, 5], np.array([[1., 2.],[3., 4.]]), np.array([[1.],[2.]]), 435.0, 234, 1.0, 0.123, 2334] 
+    DataFromUser = [2, [4, 5], np.array([[1., 2.],[3., 4.]]), np.array([[1.],[2.]]), 435.0, 10, 1.0, 0.123, 2334] 
     # Exeplary data - uncomment numpy import to use
 
     # DataFromUser = ReadData().PerformReadingSequence()   #Uncomment later
